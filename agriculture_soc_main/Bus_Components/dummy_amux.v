492530a4d870c0c328c40ee59fb8b2b528c57bc8
sandinj <112848317+sandinj@users.noreply.github.com>
2025-01-19 14:06:01 +1100

    added debug output

--- agriculture_soc_main/Bus_Components/dummy_amux.v
+++ agriculture_soc_main/Bus_Components/dummy_amux.v
@@ -6,5 +6,9 @@ module dummy_amux
     input wire [$clog2(AMUX_INPUTS)-1:0] INPUT_SEL,
     output wire ANALOG_PASSTHROUGH   // This is an analog wire
 );
-   // TODO: Implement the AMUX logic here     
+    // TODO: Implement the AMUX logic here  
+
+    always @(INPUT_SEL) begin
+        $display("INPUT_SEL = %h", INPUT_SEL);
+    end
 endmodule
