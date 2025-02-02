9d7037380b38171b88b354ae8c48814e4f09bfe1
sandinj <112848317+sandinj@users.noreply.github.com>
2025-02-02 15:08:46 +1100

    added debug output on reset

--- agriculture_soc_main/Bus_Components/dummy_amux.v
+++ agriculture_soc_main/Bus_Components/dummy_amux.v
@@ -5,14 +5,15 @@ module dummy_amux
 (
     input wire [$clog2(AMUX_INPUTS)-1:0] INPUT_SEL,
     output wire ANALOG_PASSTHROUGH,   // This is an analog wire
-    input wire clk
+    input wire clk,
+    input wire reset
 );
 
     reg [$clog2(AMUX_INPUTS)-1:0] INPUT_SEL_PREV;
 
     // TODO: Implement the AMUX logic here  
 
-    always @(posedge clk) begin
+    always @(posedge clk or posedge reset) begin
         if (INPUT_SEL != INPUT_SEL_PREV) begin
             $display("INPUT_SEL = %h", INPUT_SEL);
             INPUT_SEL_PREV <= INPUT_SEL;
