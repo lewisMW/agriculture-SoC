0e7a0b86951a26f2d39ec51c4a0a702afd5b6a0e
Ben S <b3nschwarz@gmail.com>
2025-02-16 14:03:23 +1100

    added paths.mk to git ignore

--- paths.mk
+++ paths.mk
@@ -1,2 +1,3 @@
 ### Change this to your DesignStart directory:
-DESIGNSTART_DIR = /mnt/d/AT510-MN-80001-r2p0-00rel0
+# DESIGNSTART_DIR = /mnt/d/AT510-MN-80001-r2p0-00rel0
+DESIGNSTART_DIR = /media/ben/SpareDrive/SocCompetition/AT510-MN-80001-r2p0-00rel0
