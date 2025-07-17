27a90f1a450d9d2df1718f9f7700305c6460c647
lewisMW <lewismaxwatts@yahoo.co.uk>
2025-07-17 22:07:37 +1000

    Add slcorem0_tech to pull .sh

--- nanosoc/subtree_pull.sh
+++ nanosoc/subtree_pull.sh
@@ -5,3 +5,5 @@ git subtree pull --prefix=nanosoc/generic_lib_tech https://git.soton.ac.uk/socla
 git subtree pull --prefix=nanosoc/nanosoc_tech https://git.soton.ac.uk/soclabs/nanosoc_tech.git main --squash
 git subtree pull --prefix=nanosoc/rtl_primitives_tech https://git.soton.ac.uk/soclabs/rtl_primitives_tech.git main --squash
 git subtree pull --prefix=nanosoc/soctools_flow https://git.soton.ac.uk/soclabs/soctools_flow.git main --squash
+
+git subtree pull --prefix=nanosoc/nanosoc_tech/nanosoc/slcorem0_tech https://git.soton.ac.uk/soclabs/slcorem0_tech.git main --squash
