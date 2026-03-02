ba22cdf9d590fa0a933b184f79350905fbd04454
HeeChan <moiseskim98@gmail.com>
2026-03-03 09:07:34 +1100

    dac block, fsm not yet

---
+++ analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1772456203 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_AFE/fsm/functional/verilog.v
+1769828266 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_probe/dig_sti_tb/verilog/verilog.v
