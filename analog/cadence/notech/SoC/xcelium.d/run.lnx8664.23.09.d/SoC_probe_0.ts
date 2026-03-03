ddb0cc7cc9f66637ab1828fb9041db7cccdebbfc
HeeChan <moiseskim98@gmail.com>
2026-03-03 15:30:10 +1100

    sar fsm added and clean up of the repo

--- analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/SoC_probe_0.ts
+++ analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/SoC_probe_0.ts
@@ -1,1 +1,1 @@
-1771621672 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_probe/cap_array_8b/verilogams/verilog.vams
+1772490277 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_probe/dig_sti_tb/verilog/verilog.v
--- analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/files.ts
+++ analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/files.ts
@@ -1,2 +1,2 @@
-1771805278 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/cds.lib
+1772510141 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/cds.lib
 1769829834 /home/hee/cadence/SoC/cds.lib
