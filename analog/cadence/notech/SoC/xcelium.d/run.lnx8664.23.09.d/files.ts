410010c0d77a9b1602c2bc3023fce8aafe72f14b
HeeChan <moiseskim98@gmail.com>
2026-03-13 12:29:14 +1100

    clean up for analog directories, and creation of all behavioural blocks

--- analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/files.ts
+++ analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/files.ts
@@ -1,2 +1,2 @@
-1772510141 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/cds.lib
+1772677487 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/cds.lib
 1769829834 /home/hee/cadence/SoC/cds.lib
