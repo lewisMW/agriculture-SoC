e020dcbea51345b47851a1b3e69f64e30ba480f7
HeeChan <moiseskim98@gmail.com>
2026-03-13 12:29:14 +1100

    clean up for analog directories, and creation of all behavioural blocks

--- analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
+++ analog/cadence/notech/SoC/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -1,2 +1,3 @@
+1773282477 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/adc_tb_lib/tb_cap/verilogams/verilog.vams
 1772456203 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_AFE/fsm/functional/verilog.v
 1769828266 /home/hee/projects/agriculture-SoC/analog/cadence/notech/SoC/SoC_probe/dig_sti_tb/verilog/verilog.v
