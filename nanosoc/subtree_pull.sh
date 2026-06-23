5b2ef0c2e057a4772f9a348ab787dcda44663ec8
Daniel Geng <gengruize@gmail.com>
2026-06-23 22:21:51 +1000

    update subtree pull to include new synth scripts

--- nanosoc/subtree_pull.sh
+++ nanosoc/subtree_pull.sh
@@ -1,4 +1,5 @@
 git subtree pull --prefix=nanosoc/accelerator_wrapper_tech https://git.soton.ac.uk/soclabs/accelerator_wrapper_tech.git main --squash
+git subtree pull --prefix=nanosoc/asic_flow https://git.soton.ac.uk/soclabs/asic_flow.git main --squash
 git subtree pull --prefix=nanosoc/asic_lib_tech https://git.soton.ac.uk/soclabs/asic_library_tech.git main --squash
 git subtree pull --prefix=nanosoc/fpga_lib_tech https://git.soton.ac.uk/soclabs/fpga_lib_tech.git main --squash
 git subtree pull --prefix=nanosoc/generic_lib_tech https://git.soton.ac.uk/soclabs/generic_lib_tech.git main --squash
