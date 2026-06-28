029961f246fcaccbd2d821d50aeddd219e49dad2
Daniel Geng <rgen7310@uni.sydney.edu.au>
2026-06-28 16:24:17 +1000

    copy asic scripts for edit, set to correct paths

---
+++ ASIC_ours/Cadence/scripts/floorplan.tcl
@@ -0,0 +1,27 @@
+#------------------------------------------------------------------------------------
+# Cadence Innovus: Floorplan
+# A joint work commissioned on behalf of SoC Labs, under Arm Academic Access license.
+#
+# Contributors
+#
+# Daniel Newbrook (d.newbrook@soton.ac.uk)
+# Copyright (c) 2026, SoC Labs (www.soclabs.org)
+#------------------------------------------------------------------------------------
+create_floorplan -die_size_by_io_height max -site CoreSite -core_size 3600 5200 50 50 50 50
+
+
+read_io_file ../scripts/nanosoc_io_plan.io
+
+# these are for the SRAMS. I don't think I need to further touch this
+create_relative_floorplan -ref_type core_boundary -orient R0 -horizontal_edge_separate {1  -250  1} -vertical_edge_separate {0  400  0} -place u_nanosoc_chip_u_system_u_ss_expansion_u_region_expram_l_u_expram_l_u_sram_genblk1.u_sram
+create_relative_floorplan -ref_type object -horizontal_edge_separate {3  -250  1} -vertical_edge_separate {3  0  3} -place u_nanosoc_chip_u_system_u_ss_expansion_u_region_expram_h_u_expram_h_u_sram_genblk1.u_sram -ref u_nanosoc_chip_u_system_u_ss_expansion_u_region_expram_l_u_expram_l_u_sram_genblk1.u_sram
+
+create_relative_floorplan -ref_type core_boundary -orient R180 -horizontal_edge_separate {1  -250  1} -vertical_edge_separate {2  -400  2} -place u_nanosoc_chip_u_system_u_ss_cpu_u_region_imem_0_u_imem_0_u_sram_genblk1.u_sram
+create_relative_floorplan -ref_type object -orient R180 -horizontal_edge_separate {3  -250  1} -vertical_edge_separate {3  0  3} -place u_nanosoc_chip_u_system_u_ss_cpu_u_region_dmem_0_u_dmem_0_u_sram_genblk1.u_sram -ref u_nanosoc_chip_u_system_u_ss_cpu_u_region_imem_0_u_imem_0_u_sram_genblk1.u_sram
+
+create_place_halo -halo_deltas {4.8 4.8 2.4 4.8} -insts u_nanosoc_chip_u_system_u_ss_expansion_u_region_expram_l_u_expram_l_u_sram_genblk1.u_sram
+create_place_halo -halo_deltas {4.8 4.8 2.4 4.8} -insts u_nanosoc_chip_u_system_u_ss_expansion_u_region_expram_h_u_expram_h_u_sram_genblk1.u_sram
+create_place_halo -halo_deltas {4.8 4.8 2.4 4.8} -insts u_nanosoc_chip_u_system_u_ss_cpu_u_region_imem_0_u_imem_0_u_sram_genblk1.u_sram
+create_place_halo -halo_deltas {4.8 4.8 2.4 4.8} -insts u_nanosoc_chip_u_system_u_ss_cpu_u_region_dmem_0_u_dmem_0_u_sram_genblk1.u_sram
+
+# TODO add the analog floorplan parts
